b577abc3a756b30634b198d025244d19fbf678d0
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-12-19 01:19:30 +0600

    all function added

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function changeQuantity(amount) {
 }
 
 // function to change the size
-
 function changeSize(sizeValue) {
   size = sizeValue; // get the new size
 }
@@ -54,5 +53,99 @@ function addToCart() {
     totalPrice,
   };
 
-  console.log(product);
+  cart.push(product); // add the product to the cart
+
+  const checkoutButton = document.getElementById("checkoutButton");
+
+  checkoutButton.classList.remove("hidden"); // remove the hidden class
+  checkoutButton.classList.add("flex"); // show the checkout button
+
+  const totalCart = document.getElementById("totalCart");
+
+  totalCart.innerHTML = cart.length; // update the total cart items
+}
+
+// funciton to checkout
+function checkout() {
+  const modal = document.getElementById("modal"); // get the modal
+
+  modal.classList.remove("hidden"); // remove the hidden class
+  modal.classList.add("flex"); // show the modal
+
+  const cartDetails = document.getElementById("cart_details");
+
+  cartDetails.innerHTML = ""; // clear the cart details
+
+  cart.reverse().map(
+    (item) =>
+      (cartDetails.innerHTML += `
+              <li class="flex items-center border-b border-[#DBDFEA] py-4">
+                <div class="w-[278px] flex items-center gap-[7.36px]">
+                  <img
+                    src="${item.thumbnail}"
+                    alt="watvh image"
+                    width="36.643px"
+                    height="41.786px"
+                    class="rounded-[3px]"
+                  />
+
+                  <p
+                    class="text-[#364A63] font-[roboto] text-[14px] leading-[165%]"
+                  >
+                    Classy Modern Smart watch
+                  </p>
+                </div>
+
+                <p
+                  class="text-[#364A63] font-[roboto] text-[14px] leading-[165%] w-[62px] text-center"
+                >
+                  ${
+                    item.brandColor.charAt(0).toUpperCase() +
+                    item.brandColor.slice(1)
+                  }
+                </p>
+
+                <p
+                  class="text-[#364A63] font-[roboto] text-[14px] leading-[165%] w-[69px] text-center"
+                >
+                  ${item.size}
+                </p>
+
+                <p
+                  class="text-[#364A63] font-[roboto] text-[14px] leading-[165%] w-[59px] text-center"
+                >
+                  ${item.quantity}
+                </p>
+
+                <p
+                  class="text-[#364A63] font-[roboto] text-[14px] leading-[165%] w-[91px] text-right"
+                >
+                  $${item.totalPrice}
+                </p>
+              </li>
+    `)
+  );
+}
+
+// funciton continue shoping
+function continueShopping() {
+  const modal = document.getElementById("modal"); // get the modal
+
+  modal.classList.remove("flex"); // hide the modal
+  modal.classList.add("hidden"); // show the modal
+}
+
+// function to close modal
+function closeModal() {
+  const modal = document.getElementById("modal"); // get the modal
+
+  modal.classList.remove("flex"); // hide the modal
+  modal.classList.add("hidden"); // show the modal
+
+  const checkoutButton = document.getElementById("checkoutButton");
+
+  checkoutButton.classList.remove("flex"); // hide the checkout button
+  checkoutButton.classList.add("hidden"); // show the checkout button
+
+  cart = []; // empty the cart
 }
